1d90b62325c10d61458fe96426c78124940780e9
FrankCB <FrankCB>
2016-11-08 15:06:53 +0100

    fourth

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -19,3 +19,57 @@ $hamburger.on("click", function(e){
     $(".main").toggleClass("show-menu");
   }
 });
+
+
+/*      HEADER HIDDEN OR NOT      */
+
+var didScroll;
+var lastScrollTop = 0;
+var delta = 5;
+var headerHeight = $('header').outerHeight();
+
+$(window).scroll(function(event){
+  didScroll = true;
+
+});
+
+setInterval(function(){
+  if(didScroll){
+    hasScrolled();
+    didScroll = false;
+  }
+}, 250);
+
+
+function hasScrolled(){
+  var wScroll = $(this).scrollTop();
+
+  if(Math.abs(lastScrollTop - wScroll) <= delta )
+    return;
+
+
+  if(wScroll > lastScrollTop && wScroll > headerHeight){
+    $('header').addClass('is-hidden');
+  }else{
+    if(wScroll + $(window).height() < $(document).height()){
+      $('header').removeClass('is-hidden');
+      $('header').addClass('on-scrolling');
+    }
+  }
+
+  lastScrollTop = wScroll;
+
+  if(wScroll == 0){
+    $('header').removeClass('on-scrolling');
+  }
+}
+
+$(window).scroll(function(){
+
+  var wScroll = $(this).scrollTop();
+
+  if(wScroll > $('.inner-about').offset().top -
+  ($(window).height() / 1.5 )){
+    $("#svg path").addClass("svg");
+  }
+});
